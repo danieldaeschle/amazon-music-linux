9ead92aa2a8fd4a6de09946c694602716e7e26cc
Daniel Däschle <daniel.daeschle@gmail.com>
2018-03-23 16:34:01 +0100

    Add media button support

--- src/main/index.js
+++ src/main/index.js
@@ -1,8 +1,7 @@
-'use strict'
-
-import { app, BrowserWindow, session } from 'electron'
+import { app, BrowserWindow, session, globalShortcut, ipcMain } from 'electron'
 import * as path from 'path'
 import { format as formatUrl } from 'url'
+import DBus from 'dbus'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -66,4 +65,107 @@ app.on('activate', () => {
 // create main BrowserWindow when electron is ready
 app.on('ready', () => {
   mainWindow = createMainWindow()
+  registerShortcuts()
 })
+
+function registerShortcuts() {
+  // MediaNextTrack, MediaPreviousTrack, MediaStop und MediaPlayPause
+  try {
+    const dbus = new DBus();
+    const session = dbus.getBus('session');
+  
+    registerBindings('gnome', session);
+    registerBindings('mate', session);
+  } catch (e) { }
+  globalShortcut.register('MediaNextTrack', () => {
+    nextTrack()
+  })
+  globalShortcut.register('MediaPreviousTrack', () => {
+    nextTrack()
+  })
+  globalShortcut.register('MediaStop', () => {
+    stopMusic()
+  })
+  globalShortcut.register('MediaPlayPause', () => {
+    startStopMusic()
+  })
+}
+
+// fix for gnome and mate
+function registerBindings(desktopEnv, session) {
+  session.getInterface(`org.${desktopEnv}.SettingsDaemon`,
+  `/org/${desktopEnv}/SettingsDaemon/MediaKeys`,
+  `org.${desktopEnv}.SettingsDaemon.MediaKeys`, (err, iface) => {
+    if (!err) {
+      iface.on('MediaPlayerKeyPressed', (n, keyName) => {
+        switch (keyName) {
+          case 'Next': nextTrack(); return;
+          case 'Previous': previousTrack(); return;
+          case 'Play': startStopMusic(); return;
+          case 'Stop': stopMusic(); return;
+          default: return;
+        }
+      });
+      iface.GrabMediaPlayerKeys(0, `org.${desktopEnv}.SettingsDaemon.MediaKeys`); // eslint-disable-line
+    }
+  });
+}
+
+function nextTrack() {
+  const clickEvent = `
+    var clickEvent = new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': false
+    });`
+  const next = `
+    ${clickEvent}
+    var element = document.getElementsByClassName('nextButton')[0];
+    element.dispatchEvent(clickEvent);`
+  mainWindow.webContents.executeJavaScript(next)
+}
+
+function stopMusic() {
+  const clickEvent = `
+    var clickEvent = new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': false
+    });`
+  const next = `
+    ${clickEvent}
+    var element = document.getElementsByClassName('playButton')[0];
+    var classes = element.classList;
+    if (classes.contains('playerIconPause')) {
+      element.dispatchEvent(clickEvent);
+    }`
+  mainWindow.webContents.executeJavaScript(next)
+}
+
+function startStopMusic() {
+  const clickEvent = `
+    var clickEvent = new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': false
+    });`
+  const next = `
+    ${clickEvent}
+    var element = document.getElementsByClassName('playButton')[0];
+    element.dispatchEvent(clickEvent);`
+  mainWindow.webContents.executeJavaScript(next)
+}
+
+function previousTrack() {
+  const clickEvent = `
+    var clickEvent = new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': false
+    });`
+  const next = `
+    ${clickEvent}
+    var element = document.getElementsByClassName('previousButton')[0];
+    element.dispatchEvent(clickEvent);`
+  mainWindow.webContents.executeJavaScript(next)
+}
